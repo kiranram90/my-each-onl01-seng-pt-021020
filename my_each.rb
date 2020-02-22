471697c62d822a30bdd803ed7c0a385de2c233e5
kiran ram <kram@lions.molloy.edu>
2020-02-22 16:15:44 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,8 @@
-
-
-def hello(array)
+def my_each(array)
   i = 0
-  collection = []
   while i < array.length
-    collection << yield(array[i])
-    i += 1
+    yield(array[i])
+    i = i + 1
   end
-  collection
 end
+
